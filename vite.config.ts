27b4a7480d41d4d041e2941d2b6582ae8848d403
fahdgamad080 <2201129@horus.edu.eg>
2025-06-10 21:13:29 +0300

    chore: update project name and homepage, add gh-pages dependency, and set base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fahds-code",
   server: {
     host: "::",
     port: 8080,
